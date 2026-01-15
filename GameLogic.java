868ceab216c3b406c9cf84dc3a8f3173467f7c7d
Latheka R <latheka12r@gmail.com>
2026-01-15 22:01:55 +0530

    Add GameLogic class with checkWinner method

---
+++ GameLogic.java
@@ -0,0 +1,12 @@
+package sample;
+
+import javax.swing.*;
+
+public class GameLogic {
+
+    public String checkWinner(JButton[] buttons) {
+        // Check winning combinations
+        // Return "X", "O", or null
+        return null;
+    }
+}
